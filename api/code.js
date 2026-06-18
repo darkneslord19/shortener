dc4e3fac6ac163cefefa988f8c7d854b05851815
darkneslord19 <guvenkaratas.2025@gmail.com>
2026-06-18 18:52:04 +0300

    Create code.js

---
+++ api/code.js
@@ -0,0 +1,26 @@
+const MAP = {
+  "2T5AF8G": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/welcome.json"
+};
+
+export default async function handler(req, res) {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const code = url.searchParams.get('code');
+  
+  if (!code) {
+    return res.status(400).json({ error: "Lütfen bir kod girin" });
+  }
+
+  const targetUrl = MAP[code];
+  
+  if (!targetUrl) {
+    return res.status(404).json({ error: "Bu kod bulunamadı" });
+  }
+
+  try {
+    const response = await fetch(targetUrl);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Bir hata oluştu" });
+  }
+}
