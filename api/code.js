374bfce771da35a0fddf5d7d9fb8da895bd98ffd
darkneslord19 <guvenkaratas.2025@gmail.com>
2026-06-19 17:19:00 +0300

    Update code.js

--- api/code.js
+++ api/code.js
@@ -1,9 +1,5 @@
 const MAP = {
-  "NU88U7I": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/Merhaba.json",
-  "Z2VU9NT": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/welcome.json",
-  "9OWRSBM": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/Shortener.json",
-  "VO1WCJV": "https://raw.githubusercontent.com/darkneslord19/shortener/main/pastes/Mer.json",
-  "IUDAQ1R": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/Zenk.json"
+  "Z2VU9NT": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/welcome.json"
 };
 
 export default async function handler(req, res) {
