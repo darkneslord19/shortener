09378e9df004a754a8bcd31bff2d33ec885baab7
darkneslord19 <guvenkaratas.2025@gmail.com>
2026-06-19 21:11:09 +0300

    🔗 Link güncellemesi: 2 link

--- api/code.js
+++ api/code.js
@@ -1,7 +1,6 @@
 const MAP = {
   "NU88U7I": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/Merhaba.json",
-  "Z2VU9NT": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/welcome.json",
-  "VO1WCJV": "https://raw.githubusercontent.com/darkneslord19/shortener/main/pastes/Mer.json"
+  "Z2VU9NT": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/welcome.json"
 };
 
 export default async function handler(req, res) {
