01b2ac41eea17f83e0b213d49b241e8e2b3670ea
darkneslord19 <guvenkaratas.2025@gmail.com>
2026-06-19 19:35:01 +0300

    🔗 Link güncellemesi: 1 link

--- api/code.js
+++ api/code.js
@@ -1,6 +1,5 @@
 const MAP = {
-  "3NSE64L": "https://raw.githubusercontent.com/darkneslord19/shortener/main/pastes/Mer.json",
-  "CGHHS97": "https://raw.githubusercontent.com/darkneslord19/shortener/main/pastes/Met.json"
+  "YP06I5M": "https://raw.githubusercontent.com/darkneslord19/shortener/main/pastes/Mer.json"
 };
 
 export default async function handler(req, res) {
