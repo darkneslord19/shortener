1ba8107604b3bdc8a0273411541030da72bf303a
darkneslord19 <guvenkaratas.2025@gmail.com>
2026-06-19 20:37:15 +0300

    🔗 Link güncellemesi: 9 link

--- api/code.js
+++ api/code.js
@@ -1,8 +1,13 @@
 const MAP = {
-  "NU88U7I": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/Merhaba.json",
-  "Z2VU9NT": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/welcome.json",
-  "VO1WCJV": "https://raw.githubusercontent.com/darkneslord19/shortener/main/pastes/Mer.json",
-  "PL06JI8": "https://raw.githubusercontent.com/darkneslord19/shortener/main/pastes/Met.json"
+  "1VSS1DG": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/Shortener.json",
+  "2MOR1GS": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/welcome.json",
+  "KWQKDRQ": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/welcome.json",
+  "NZTPHNA": "https://raw.githubusercontent.com/darkneslord19/darkneslord/main/pastes/Dark.json",
+  "BSRFPW2": "https://raw.githubusercontent.com/darkneslord19/shortener/main/pastes/Met.json",
+  "5IT7AVC": "https://raw.githubusercontent.com/darkneslord19/shortener/main/pastes/Mer.json",
+  "VPQS1WC": "https://raw.githubusercontent.com/darkneslord19/shortener/main/pastes/Yet.json",
+  "8333Y4H": "https://raw.githubusercontent.com/darkneslord19/shortener/main/pastes/Fat.json",
+  "QX4ZNOZ": "https://raw.githubusercontent.com/darkneslord19/shortener/main/pastes/Ger.json"
 };
 
 export default async function handler(req, res) {
